1a7fc7dbbd548214d48a03044111615a2de32cf8
Lammert Hilarides <lammert.hilarides@gmail.com>
2022-11-07 10:33:43 +0100

    fix build errors

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+        resolve: {fullySpecified: false}
+      }
+    ]
+  }
+};
+
+module.exports = config;
